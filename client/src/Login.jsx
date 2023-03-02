26a8b5b31e4f1591d12b0d0f54b5a4eda6488ae3
Brandon Vega <vegabrandon1250@gmail.com>
2023-03-01 22:10:46 -0500

    Added login styling as well as axios post request skeleton

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
-
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const submit = () => {
+    axios.post('localhost:5000/login', {username, password})
+      .then(data => {
+        
+      })
+  }
   return (
     <>
       <h1 className='text-white text-[5rem]'>Login</h1>
-      <input type="text" />
-      <input type="text" />
+      <label className='text-white text-2xl' htmlFor="username">Username</label><br />
+      <input onChange={(e) => {setUsername(e.target.value)}} name='username' type="text" className='mb-5'/>
+      <br />
+      <label className='text-white text-2xl' htmlFor="password">Password</label><br />
+      <input onChange={(e) => {setPassword(e.target.value)}} name='password' type="text" />
+      <br />
+      <button onClick={submit} className="bg-white p-2 mt-5 rounded-xl btn">Submit</button>
+      <button onClick={submit} className="bg-white p-2 mt-5 rounded-xl ml-2 btn">Signup</button>
     </>
   )
 }
