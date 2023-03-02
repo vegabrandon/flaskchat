5f185cffdc7c445472276053426b38e0dd1c7374
Brandon Vega <vegabrandon1250@gmail.com>
2023-03-01 22:02:27 -0500

    Added structure for login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './Login';
 function App() {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({})
+
   useEffect(() => {
 
   }, [])
   return (
     <div className="App">
-      test
-      <Login />
+
+        {isLoggedIn ? <>
+          <h1 className='text-white text-[5rem]'>
+            Welcome {user.username}!
+          </h1>
+        </> : <Login />}
+
     </div>
   );
 }
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -4,7 +4,9 @@ const Login = () => {
 
   return (
     <>
-      Login
+      <h1 className='text-white text-[5rem]'>Login</h1>
+      <input type="text" />
+      <input type="text" />
     </>
   )
 }
