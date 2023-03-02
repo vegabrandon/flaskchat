7dc47f9653818b32b99ae68446a27b89a843e18a
Brandon Vega <vegabrandon1250@gmail.com>
2023-03-02 16:30:53 -0500

    Added tailwind elements

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -6,6 +6,39 @@ const Login = ({setIsLoggedIn, setUser}) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [modalOpen, setModalOpen] = useState(false)
+  const [signupPassword, setSignupPassword] = useState('')
+  const [signupUsername, setSignupUsername] = useState('')
+
+  var signupStyles = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(255, 255, 255, 0.4)"
+    },
+    content: {
+      position: "absolute",
+      textAlign: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-evenly',
+      borderRadius: '20px',
+      margin: "auto",
+      width: "70vw",
+      maxWidth: '480px',
+      bottom: "45vh",
+      border: "1px solid #ccc",
+      background: "#fff",
+      overflow: "auto",
+      WebkitOverflowScrolling: "touch",
+      outline: "none",
+      padding: "20px",
+
+    }
+  };
+
   const submit = () => {
     axios.post('http://127.0.0.1:5000/login', {username, password})
       .then(data => {
@@ -19,11 +52,19 @@ const Login = ({setIsLoggedIn, setUser}) => {
   return (<>
     <Modal
       isOpen={modalOpen}
+      appElement={document.getElementById('App')}
+      style={signupStyles}
     >
-      Hello!
-      <button
-        onClick={() => {setModalOpen(false)}}
-      >Close</button>
+      <h2 className='text-2xl'><b>Signup</b></h2>
+        <h4>Username</h4>
+        <input className='' type="text" onChange={e => setSignupUsername(e.target.value)}/>
+
+        <h4>Password</h4>
+        <input className='' type="password" onChange={e => setSignupPassword(e.target.value)} />
+        <div className='flex-row'>
+          <button className='' onClick={() => {}}>Submit</button>
+          <button className='' onClick={() => {setModalOpen(false)}}>Close</button>
+        </div>
     </Modal>
     <h1 className='text-white text-[5rem]'>Login</h1>
     <div className='h-[50vh] flex flex-col justify-center'>
