7578f2c6bee737db29afb9d208aa9aeb2894a16d
Brandon Vega <vegabrandon1250@gmail.com>
2023-03-01 21:30:23 -0500

    Added server code to support a post request

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React, { useEffect } from 'react';
-
+import Login from './Login';
 function App() {
+  
   useEffect(() => {
 
   }, [])
   return (
     <div className="App">
       test
+      <Login />
     </div>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,27 @@
-from flask import Flask;
+from flask import Flask, render_template, redirect, request, session, send_from_directory
+from flask_session import Session
+import os
+from pymongo import MongoClient
 
-app = Flask(__name__)
+# Flask Server
+app = Flask(__name__, static_folder='../client/public')
+
+# MongoDB Client
+client = MongoClient('localhost', 27017)
+db = client.flask_db
+users = db.users
 
 # API Route in Flask
-
 @app.route('/test', methods=['GET'])
+
 def test():
-    return {"test": {"test": "test"}}
+  return {"test": {"test": "test"}}
+
+@app.route('/users', methods=['POST'])
+def users():
+   if request.method=='POST':
+      
+
 
 if __name__ == "__main__":
     app.run(debug=True)
